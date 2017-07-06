eaee8f947058d5c274bdb02cd43daaae13320600
ruSl0n <bakirovr@yandex.ru>
2017-07-06 20:17:15 +0300

    6.07

---
+++ Game/app.js
@@ -0,0 +1,18 @@
+var express = require('express')();
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var path = require('path');
+var game  = require('./model/game.js');
+
+server.listen(8080);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/public/html/index.html');
+});
+
+io.on('connection', function(socket){
+	game.init(socket);
+});
---
+++ Game/model/game.js
@@ -0,0 +1,3 @@
+exports.init = function(socket){
+	socket.emit('show_start_form');
+}
